926ed1bf2f4ed76c9ec7baa6672b67880071b231
rrc <rrc@DESKTOP-70G21MP>
2025-04-04 20:47:55 +0545

    2ndProgramInitiated

---
+++ java/src/com/java/practice/SecondMyJava.java
@@ -0,0 +1,17 @@
+package com.java.practice;
+
+public class SecondMyJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			    int x = 20;
+			    int y = 18;
+			    if (x > y) {
+			      System.out.println("x is greater than y");
+		
+			}
+
+	}
+
+}
